38071a79978180cb8d191a38812b91271d951b88
rom <rommel.p@ecb.co.il>
2023-04-23 16:58:33 +0800

    update backend endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // API_URL: 'https://api.inhistime041423.com/',
-  API_URL: 'http://44.211.74.9:3000/'
+  API_URL: 'http://api.romgotaperfectcathch.com'
 };
